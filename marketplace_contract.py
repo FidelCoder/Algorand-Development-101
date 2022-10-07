7b50024441cda43a2f1368bc062b4a34683130ed
Griffins-sys254 <griffinesonyango@gmail.com>
2022-10-07 09:28:36 +0300

    contract for market place

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -1,62 +1,70 @@
+#import PyTeal
 from pyteal import *
 
-
+# create class products required for the smart contracts
 class Product:
-    #Adding global state variable
-    class variables:
-        name = Bytes('NAME')
-        image = Bytes('IMAGE')
-        description =  Bytes('DESCRIPTION')
-        price = Bytes('PRICE')
-        location = Bytes('LOCATION')
-        sold = Bytes('SOLD')
+    class Variables:
+        name = Bytes("NAME")
+        image = Bytes("IMAGE")
+        description = Bytes("DESCRIPTION")
+        price = Bytes("PRICE")
+        sold = Bytes("SOLD")
 
     class AppMethods:
-        buy = Bytes('BUY')
-        #Create a Product
-        def application_creation(self):
-            return seq([
-                Assert(Txn.application_args.length() == Int(4)),
-                Assert(Txn.note() == Bytes("tutorial-marketplace:uv1")),
-                Assert(Btoi(Txn.application_args[3]) > Int(0)),
-                App.globalPut(self.Variables.name, Txn.application_args[0]),
-                App.globalPut(self.Variables.image, Txn.application_args[1]),
-                App.globalPut(self.Variables.description, Txn.application_args[2]),
-                App.globalPut(self.variable.price,Txn.application_args[3]),
-                App.globalPut(self.variable.sold), Int(0)),
-                Approve()
-            ])
+        buy = Bytes("buy")
 
-        #Buy a product
-        def buy(self):
-            count = Txn.application_args[1]
-            valid_number_of_transactions = Global.group_size() == Int(2)
+    #create products
+    def application_creation(self):
+        return Seq([
+            Assert(Txn.application_args.length() == Int(4)),
+            Assert(Txn.note() == Bytes("tutorial-marketplace:uv1")),
+            Assert(Btoi(Txn.application_args[3]) > Int(0)),
+            App.globalPut(self.Variables.name, Txn.application_args[0]),
+            App.globalPut(self.Variables.image, Txn.application_args[1]),
+            App.globalPut(self.Variables.description, Txn.application_args[2]),
+            App.globalPut(self.Variables.price, Btoi(Txn.application_args[3])),
+            App.globalPut(self.Variables.sold, Int(0)),
+            Approve()
+        ])
 
-            valid_payment_to_seller = And(
-                Gtxn[1].type_enum() == TxnType.Payment,
-                Gtxn[1].receiver() == Global.creator_address(),
-                Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
-                Gtxn[1].sender() == Gtxn[0].sender(),
-            )
-            can_buy = And(valid_number_of_transactions,
-            valid_payment_to_seller)
+    #buy products
+    def buy(self):
+        count = Txn.application_args[1]
+        valid_number_of_transactions = Global.group_size() == Int(2)
 
-            update_state = Seq([
-                App.globalPut(self.Variables.sold,App.globalGet(self.Variables.sold) + Btoi(count)),
-                Approve()
-            ])
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == Global.creator_address(),
+            Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
+            Gtxn[1].sender() == Gtxn[0].sender(),
+        )
 
-            return if(can_buy).Then(update_state).Else(Reject())
-        
-        #Deleting a Product
-        def application_deletion():
-            return Return(Txn.sender() == Global.creator_address())
+        can_buy = And(valid_number_of_transactions,
+                      valid_payment_to_seller)
 
-        def application_start(self):
-            return Cond(
-                [Txn.application_id() == Int(0), self.application_creation()],
-                [Txn.on_completion_args[0] == self.AppMethods.buy, self.buy()],
-                [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
-            )
-        
+        update_state = Seq([
+            App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Btoi(count)),
+            Approve()
+        ])
 
+        return If(can_buy).Then(update_state).Else(Reject())
+
+    #Delete products
+    def application_deletion(self):
+        return Return(Txn.sender() == Global.creator_address())
+
+    #Check transaction Condition
+    def application_start(self):
+        return Cond(
+            [Txn.application_id() == Int(0), self.application_creation()],
+            [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
+            [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
+        )
+
+    #approval program
+    def approval_program(self):
+        return self.application_start()
+
+    #clear program
+    def clear_program(self):
+        return Return(Int(1))
